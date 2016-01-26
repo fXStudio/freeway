da72fea7a8dcf70a20c3debe1f6bf05e92be65f6
Ajaxfan <ajax_2003@163.com>
2016-01-26 10:44:58 +0800

    Add FreeMarker Engine

--- FreeWayPro/src/main/java/com/freeway/web/controllers/GenericController.java
+++ FreeWayPro/src/main/java/com/freeway/web/controllers/GenericController.java
@@ -1,11 +1,18 @@
 package com.freeway.web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 全局的拦截器
@@ -41,5 +48,54 @@ public class GenericController {
 		request.getSession().removeAttribute("freeWayUser");
 
 		return "redirect:/";
+	}
+
+	/**
+	 * Static list of users to simulate Database
+	 */
+	private static List<User> userList = new ArrayList<User>();
+
+	// Initialize the list with some data for index screen
+	static {
+		userList.add(new User("Bill", "Gates"));
+		userList.add(new User("Steve", "Jobs"));
+		userList.add(new User("Larry", "Page"));
+		userList.add(new User("Sergey", "Brin"));
+		userList.add(new User("Larry", "Ellison"));
+	}
+
+	/**
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(@ModelAttribute("model") ModelMap model) {
+
+		model.addAttribute("userList", userList);
+		model.addAttribute("scripts", Arrays.asList(new Script("main"), new Script("print")));
+
+		return "index";
+	}
+
+	/**
+	 * Add a new user into static user lists and display the same into FTL via
+	 * redirect
+	 * 
+	 * @param user
+	 * @return Redirect to /index page to display user list
+	 */
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(@ModelAttribute("user") User user) {
+
+		if (null != user && null != user.getFirstname() && null != user.getLastname() && !user.getFirstname().isEmpty()
+				&& !user.getLastname().isEmpty()) {
+
+			synchronized (userList) {
+				userList.add(user);
+			}
+
+		}
+
+		return "redirect:index.html";
 	}
 }
---
+++ FreeWayPro/src/main/java/com/freeway/web/controllers/Script.java
@@ -0,0 +1,17 @@
+package com.freeway.web.controllers;
+
+public class Script {
+	private String name;
+
+	public Script(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
