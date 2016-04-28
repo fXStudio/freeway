e23b62a8c486a22064e9d6a434a9def9d8866fa8
Ajaxfan <ajax_2003@163.com>
2016-04-28 13:24:22 +0800

    增加查看图片相关的文件

---
+++ FreeWayPro/src/main/webapp/resources/javascript/plugins/PicColumn.js
@@ -0,0 +1,14 @@
+/**
+ * 查看图片列
+ */
+Ext.define('Ext.plugins.PicColumn', {
+	extend: 'Ext.grid.column.Column',
+	alias: 'widget.piccolumn',
+	
+	header: '图片',
+	width: 50,
+	align: 'center',
+	renderer: function (data, metadata, record, rowIndex, columnIndex, store) {  
+	     return '<img src="images/pic.png"></img>'; 
+	} 
+})
